81c02109558b2f75be61e784affa5a799c48f2b8
S1moT3ch <s.cantore2@studenti.poliba.it>
2025-12-15 16:42:08 +0100

    15-12-25-16.40

--- frontend/src/components/LightDrawer.jsx
+++ frontend/src/components/LightDrawer.jsx
@@ -3,8 +3,9 @@ import {
     Box,
     Typography,
     Switch,
-    Stack,
+    Stack, Chip, ToggleButton,
 } from "@mui/material";
+import {useState} from "react";
 
 export default function LightDrawer({
                                         light,
@@ -12,27 +13,76 @@ export default function LightDrawer({
                                         toggleRelay,
                                         onClose,
                                     }) {
+
+    const [isOn, setIsOn] = useState(false);
     if (!light) return null;
+
+    const relayNames = {
+        0: "Grotta",
+        1: "Perimetro",
+        2: "Viale",
+    }
 
     const relay = relays.find(r => r.id === parseInt(light, 10));
     console.log("relay", relays);
 
     return (
-        <Drawer anchor="right" open={Boolean(light)} onClose={onClose}>
+        <Drawer anchor="right" open={Boolean(light)} onClose={onClose} sx>
             <Box sx={{ width: 280, p: 2 }}>
                 <Typography variant="h6">
-                    Luce: {light}
+                    Luce: {relayNames[relay.id] ?? `ID ${relay.id}`}
                 </Typography>
 
-                <Stack direction="row" spacing={2} alignItems="center" sx={{ mt: 2 }}>
-                    <Typography>Spento</Typography>
-                    <Switch
-                        checked={relay?.state}
-                        onChange={(e) =>
-                            toggleRelay(light, e.target.checked)
-                        }
+                <Stack direction="column" spacing={2} alignItems="center" sx={{ mt: 2 }}>
+                    <Chip
+                        sx={{
+                            width: "3rem",
+                            height: "3rem",
+                            borderRadius: "50%",
+                        }}
+                        label={relay?.state ? "1" : "0"}
+                        color={relay?.state ? "success" : "warning"}
+                        size="small"
                     />
-                    <Typography>Acceso</Typography>
+
+                    <Box>
+                        <ToggleButton
+                            value="check"
+                            onChange={() => {
+                                const newState = !isOn;
+                                setIsOn(newState);
+                                toggleRelay(light, false);
+                            }}
+                            sx={{
+                                width: "3rem",
+                                height: "3rem",
+                                fontWeight: "bold",
+                                color: "white",
+                                border: "2px solid black",
+                                backgroundColor: "red",
+                            }}
+                        >
+                            O
+                        </ToggleButton>
+                        <ToggleButton
+                            value="check"
+                            onChange={() => {
+                                const newState = !isOn;
+                                setIsOn(newState);
+                                toggleRelay(light, true);
+                            }}
+                            sx={{
+                                width: "3rem",
+                                height: "3rem",
+                                fontWeight: "bold",
+                                color: "white",
+                                border: "2px solid black",
+                                backgroundColor: "green",
+                            }}
+                        >
+                            I
+                        </ToggleButton>
+                    </Box>
                 </Stack>
             </Box>
         </Drawer>
